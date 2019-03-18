2caff171728339fa7ff61544a4a6b4807227dc8d
wangzehao1016 <zehao@connect.hku.hk>
2019-03-19 01:11:48 +0800

    modify instructor model features

--- system/models.py
+++ system/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Instructor(models.Model):
-	name=models.CharField(max_length=200)
-	UID=models.CharField(max_length=200)
+	firstname=models.CharField(max_length=200)
+	lastname=models.CharField(max_length=200)
+	username=models.CharField(max_length=200)
+	encodedPW=models.CharField(max_length=200)
 	#pwd will be handled by Django, so not included
 	email=models.EmailField()
 	autobiagraphy=models.TextField()
 	def __str__(self):
-		return f'{self.name} ({self.autobiagraphy})'
+		return f'{self.name} {self.lastname} ({self.autobiagraphy})'
