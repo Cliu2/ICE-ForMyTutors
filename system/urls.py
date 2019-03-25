9eabfe90b14a9c6c47a0266153d3cd4ed99ac2ca
Kim Li <imon247@connect.hku.hk>
2019-03-25 13:54:07 +0800

    view course list view

--- system/urls.py
+++ system/urls.py
@@ -5,14 +5,14 @@ urlpatterns = [
     # both users
     path('view/<int:user_id>/', views.showCourses, name='showCourses'),                                               # view course list
     path('view/<int:user_id>/<int:course_id>/', views.showModules, name='showModules'),                               # view module list
-    path('view/<int:user_id>/<int:course_id>/<int:module_id>', views.showComponents, name='showComponents'),          # view component list
+    #path('view/<int:user_id>/<int:course_id>/<int:module_id>/', views.showComponents, name='showComponents'),          # view component list
 
     # instructor editing mode
-    path('manage/<int:instructor_id>/<int:course_id>/',views.manageModule, name='manageModule'),                       # module list edit mode
-    path('manage/<int:instructor_id>/<int:course_id>/<int:module>/',views.manageComponent, name='showComponent'),      # component list edit mode
-    path('manage/<int:instructor_id>/<int:course_id>/<int: module>/quiz/', views.showQuizzes, name='showQuizzes'),    # add quiz view
-    path('view/<int:instructor_id>/<int:course_id>/<int:module_id>/', views.viewQuiz, name='viewQuiz'),
+    #path('manage/<int:instructor_id>/<int:course_id>/add',views.addModule, name='manageModule'),                       # module list edit mode
+    #path('manage/<int:instructor_id>/<int:course_id>/<int:module_id>/',views.manageComponent, name='showComponent'),      # component list edit mode
+    #path('manage/<int:instructor_id>/<int:course_id>/<int: module>/quiz/', views.showQuizzes, name='showQuizzes'),    # add quiz view
+    #path('view/<int:instructor_id>/<int:course_id>/<int:module_id>/', views.viewQuiz, name='viewQuiz'),
 
     # learner study mode
-    path('study/<int:learner_id>/<int:course_id>/<int:module_id>/quiz/', views.takeQuiz, name='takeQuiz'),                # learner take quiz
+    #path('study/<int:learner_id>/<int:course_id>/<int:module_id>/quiz/', views.takeQuiz, name='takeQuiz'),                # learner take quiz
 ]
--- system/views.py
+++ system/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import *
 from django.views.generic.list import ListView
+from django.views import View
 
 # Create your views here.
 
@@ -11,12 +12,12 @@ from django.views.generic.list import ListView
 """
 
 def showCourses(request, **kwargs):
-	# 
+	#
 	# 	type: determines learner page/ instructor page
 	# 	course_list: query course object enrolled by user/created by instructor, not ordered
-	# 	status: might determine display or not, or the order to display (currently a boolean object, to be changed to int) 
+	# 	status: might determine display or not, or the order to display (currently a boolean object, to be changed to int)
 	# 	user: the user exactly
-	# 
+	#
 	user_id = kwargs['user_id']
 	users = Instructor.objects.filter(id=user_id)
 	template = loader.get_template("showCourses.html")
@@ -24,13 +25,13 @@ def showCourses(request, **kwargs):
 		type = 'learner'
 		user = Learner.objects.filter(id=user_id)[0]
 		course_list = Enroll.objects.filter(learner__id=user_id).values('course')
-		status = Enroll.objects.filter(learner__id=user_id).values('status')
+		status = Enroll.objects.filter(learner__id=user_id).values('status')[0]
 
 	else:
 		user = users[0]
 		type = 'instructor'
 		course_list = Course.objects.filter(instructor__id=user_id)
-		status = course_list.values('status')
+		status = course_list.values('status')[0]
 	context = {
 		'course_list': course_list,
 		'type': type,
@@ -41,12 +42,12 @@ def showCourses(request, **kwargs):
 
 
 def showModules(request, **kwargs):
-	# 
+	#
 	# 	type: determines learner page/ instructor page
 	# 	course: a certain course enrolled by user/created by instructor that has entered
 	# 	modules: with the 'order' attribute to determine the order of display
 	# 	progress: start with 0? then progress = -1 for instructor as a dumplicate attribute, control the access of modules
-	# 
+	#
 	u_id = kwargs['user_id']
 	c_id = kwargs['course_id']
 	template = loader.get_template("showModules.html")
@@ -68,11 +69,11 @@ def showModules(request, **kwargs):
 		'progress': progress,
 	}
 	return HttpResponse(template.render(context, request))
-
+"""
 def showComponents(request, **kwargs):
-	# 
+	#
 	# 	components: a query component object in a certain module ordered by order attribute
-	# 
+	#
 	#u_id = kwargs['user_id']
 	#c_id = kwargs['course_id']
 	m_id = kwargs['module_id']
@@ -82,24 +83,31 @@ def showComponents(request, **kwargs):
 		'components': components,
 	}
 	return HttpResponse(template.render(context, request))
-
-
-
-"""
-	instructor manage course
 """
 
-def manageModule(request, **kwargs):
-	u_id = kwargs['instructor_id']
-	c_id = kwargs['course_id']
-	modules = Module.objects.filter(course__id=c_id).order_by('order')
-	template = loader.get_template("manageModule.html")
+
+
+#	instructor manage course
+
+"""
+def enterModuleInfo(request, **kwargs):
+	template = loader.get_template("enterModuleInfo.html")
 	context = {
-		'modules': modules,
+		'i_id': kwargs['instructor_id'],
+		'c_id': kwargs['course_id'],
 	}
 	return HttpResponse(template.render(context, request))
 
+class addModule(View):
+	def post(self, request):
+		course = Course.objects.filter(c_id=request.POST.get("c_id"))								# do not care order so far
+		title = request.POST.get("title")
+		new_module = Module(course=course, title=title)
+		new_module.save()
+		return showModules(user_id=request.POST.get("u_id"), course_id=request.POST.get("c_id"))
+"""
 
+"""
 def manageComponent(request, **kwargs):
 	pass
 def showQuizzes(request, **kwargs):
@@ -107,15 +115,14 @@ def showQuizzes(request, **kwargs):
 def viewQuiz(request, **kwargs):
 	pass
 
-"""
-	learner study course
-"""
+
+	#learner study course
+
 def takeQuiz(request, **kwargs):
 	pass
 
-"""
-	instructor views
-"""
+	#instructor views
+
 class ManageModule(ListView):
 	pass
 
@@ -126,12 +133,11 @@ class ShowCourses(ListView):
 	pass
 
 class showQuiz(ListView):
-	"""To be Done"""
 	pass
 
 
-"""
-	learner views
+
+	#learner views
 
 def showCourses(request, learner_id):			# course list for a learner
 	l_courses = Enroll.objects.get(learner__id=learner_id).values('course')
